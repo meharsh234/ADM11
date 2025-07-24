22ff948b64c5baa4a6d8bb9e55575489219eb1d3
2430229 <2430229@LTIN647017.cts.com>
2025-07-24 16:47:17 +0530

    Pushed

---
+++ red_face/src/ADM/red2.java
@@ -0,0 +1,10 @@
+package ADM;
+
+public class red2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ red_face/src/ADM/red3.java
@@ -0,0 +1,10 @@
+package ADM;
+
+public class red3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
